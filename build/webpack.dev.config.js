275ed6fc94e76c99448b4041e8478ac56884806c
jizongquan <2280232634@qq.com>
2020-11-15 18:53:43 +0800

    ts基础配置

---
+++ build/webpack.dev.config.js
@@ -0,0 +1,4 @@
+module.exports ={
+    //开启source-map，cheap是忽略列信息，module是会定位到ts源码，而不是js代码
+    devtool:'cheap-module-eval-source-map'
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let hello:string = 'jzq'
+document.querySelectorAll('.app')[0].innerHTML = hello;
+let bool: boolean = true
+let num:number = 123
+let str:string = 'abc'
